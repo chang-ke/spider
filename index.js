74afe9db3db4a023b71098984b897f50e94d5609
fruit-memory <315764194@qq.com>
2018-04-14 23:09:17 +0800

    增加sleep函数，每隔一段时间爬取

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const Riven = require('./util/riven');
 const cheerio = require('cheerio');
 
-function isLogin() {
-  return !!require('./user.json').cookie;
-}
+const isLogin = () => !!require('./user.json').cookie;
+
+const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
 
 async function login(request) {
   let data = await request.create({
@@ -14,18 +14,19 @@ async function login(request) {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({email: '', password: ''})
+    body: JSON.stringify({ email: '', password: '' })
   });
   const cookie = data.headers['set-cookie'];
-  const encodeToken = cookie[0]
-    .split(';')[0]
-    .split('=')[1];
-  const decodeToken = JSON.parse(new Buffer(encodeToken, 'base64').toString())
-  fs.writeFileSync('./user.json', JSON.stringify({cookie, token: decodeToken.token, userId: decodeToken.userId, clientId: decodeToken.clientId}));
+  const encodeToken = cookie[0].split(';')[0].split('=')[1];
+  const decodeToken = JSON.parse(new Buffer(encodeToken, 'base64').toString());
+  fs.writeFileSync(
+    './user.json',
+    JSON.stringify({ cookie, token: decodeToken.token, userId: decodeToken.userId, clientId: decodeToken.clientId })
+  );
 }
 
 async function getTopics(request) {
-  const {token, clientId, userId} = require('./user.json');
+  const { token, clientId, userId } = require('./user.json');
   const querystring = qs.stringify({
     src: 'web',
     uid: userId,
@@ -42,9 +43,9 @@ async function getTopics(request) {
       referer: 'https://juejin.im/timeline?sort=comment'
     }
   });
-  const body = data.body
+  const body = data.body;
   if (body.s !== 1) {
-    fs.writeFileSync('./user.json', JSON.stringify({}))
+    fs.writeFileSync('./user.json', JSON.stringify({}));
     throw new Error(body.m);
   } else {
     return body.d.entrylist;
@@ -52,18 +53,19 @@ async function getTopics(request) {
 }
 
 try {
-  (async function () {
+  (async function() {
     const request = new Riven();
     request.setDefaultOptions({
       headers: {
-        Cookie: 'gr_user_id=44868117-2a80-49e8-ba2b-2acd2a77a887; ab={}; _ga=GA1.2.1234597644.150' +
-            '6904166; MEIQIA_EXTRA_TRACK_ID=0uMtBISQ3EoiMICJMjpaZedfTBz; _gid=GA1.2.100579701' +
-            '2.1523672771; Hm_lvt_93bbd335a208870aa1f296bcd6842e5e=1521573516,1521573752,1522' +
-            '270605,1523672771; gr_session_id_89669d96c88aefbc=d54a635e-cece-4f16-aca4-808ae9' +
-            '2ee559; gr_cs1_d54a635e-cece-4f16-aca4-808ae92ee559=objectId%3A5a974f6ef265da4e8' +
-            '53d8d52; auth=; auth.sig=25Jg_aucc6SpX1VH8RlCoh6azLU; Hm_lpvt_93bbd335a208870aa1' +
-            'f296bcd6842e5e=1523675329; QINGCLOUDELB=165e4274d6090771b096025ed82d52a1ab7e48fb' +
-            '3972913efd95d72fe838c4fb|WtFwy|WtFwr'
+        Cookie:
+          'gr_user_id=44868117-2a80-49e8-ba2b-2acd2a77a887; ab={}; _ga=GA1.2.1234597644.150' +
+          '6904166; MEIQIA_EXTRA_TRACK_ID=0uMtBISQ3EoiMICJMjpaZedfTBz; _gid=GA1.2.100579701' +
+          '2.1523672771; Hm_lvt_93bbd335a208870aa1f296bcd6842e5e=1521573516,1521573752,1522' +
+          '270605,1523672771; gr_session_id_89669d96c88aefbc=d54a635e-cece-4f16-aca4-808ae9' +
+          '2ee559; gr_cs1_d54a635e-cece-4f16-aca4-808ae92ee559=objectId%3A5a974f6ef265da4e8' +
+          '53d8d52; auth=; auth.sig=25Jg_aucc6SpX1VH8RlCoh6azLU; Hm_lpvt_93bbd335a208870aa1' +
+          'f296bcd6842e5e=1523675329; QINGCLOUDELB=165e4274d6090771b096025ed82d52a1ab7e48fb' +
+          '3972913efd95d72fe838c4fb|WtFwy|WtFwr'
       }
     }); //设不设置cookie都OK的
     if (!isLogin()) {
@@ -71,9 +73,9 @@ try {
     }
     const topics = await getTopics(request);
     for (let i = 0; i < topics.length; ++i) {
-      (async function () {
-        let data = await request.get({url: topics[i].originalUrl});
-      })();
+      let data = await request.get({ url: topics[i].originalUrl });
+      await sleep(2000);
+      console.log(data.headers);
     }
   })();
 } catch (error) {
